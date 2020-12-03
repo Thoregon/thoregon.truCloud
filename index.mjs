3513a68d73f3ddd514d19f35284c43e5b14243d1
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-12-03 11:40:28 +0100

    thoregon apps

--- index.mjs
+++ index.mjs
@@ -1,5 +1,14 @@
 /**
  *
- *
+ * @module thoregon.truCloud
  * @author: blukassen
  */
+
+import Dorifer from "./lib/dorifer.mjs";
+
+import components       from './@components';
+universe.addComponents(components);
+
+export { default as ThoregonApp }   from './lib/application/thoregonapplication.mjs';
+
+export default new Dorifer();
