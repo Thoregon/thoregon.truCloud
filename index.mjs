f2b3c9d8a90a9f62a725a6f93598281d9ac46848
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-12-08 14:33:50 +0100

    embedded apps

--- index.mjs
+++ index.mjs
@@ -9,6 +9,12 @@ import Dorifer from "./lib/dorifer.mjs";
 import components       from './@components';
 universe.addComponents(components);
 
-export { default as ThoregonApp }   from './lib/application/thoregonapplication.mjs';
 
+export { default as ThoregonApplication }   from './lib/application/thoregonapplication.mjs';
+
+/*
+ * Dorifer is the service supplying the
+ * repository for the thoregon system and
+ * supporting the thoregon rules
+ */
 export default new Dorifer();
