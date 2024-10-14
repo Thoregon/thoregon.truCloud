eeb5fd5881629eb958b88827c941bd299cbdba86
Bernhard Lukassen <git@bernhard-lukassen.com>
2024-10-14 21:21:56 +0200

    longer timeout

---
+++ libold/mediathek.mjs
@@ -0,0 +1,278 @@
+/**
+ * Top level (universe wide) object as entry point
+ * to unified file descriptors and media services
+ *
+ * currently the mediathek is attached to an SSI
+ *
+ * todo:
+ *  - introduce also public mediathek items (can be used universe wide by everybody)
+ *  - introduce sharable mediatheks which can be used by
+ *      - everybody (public access)
+ *      - with credential
+ *
+ * @author: Bernhard Lukassen
+ * @licence: MIT
+ * @see: {@link https://github.com/Thoregon}
+ */
+
+import Directory                       from "/thoregon.archetim/lib/directory.mjs";
+
+import UnifiedFileDescriptor           from "./unifiedfiledescriptor.mjs";
+import HttpsStorageAdapter             from "./storage/httpsstorageadapter.mjs";
+import URLReferenceAdapter             from "./storage/urlreferenceadapter.mjs";
+import LocalStorageAdapter             from "./storage/localstorageadapter.mjs";
+import IPFSStorageAdapter              from "./storage/ipfsstorageadapter.mjs";
+import IDXDBStorageAdapter             from "./storage/idxbdstorageadapter.mjs";
+
+import { ErrFileStorageAdapterExists } from "../errors.mjs";
+
+//
+// consts
+//
+
+//
+// adapter mapping
+//
+
+const displayAdapters = new Map();
+const storageAdapters = new Map();
+
+//
+// functions (check move to polyfills)
+//
+
+const atob = globalThis.atob ?? function atob(a) {
+    return new Buffer(a, 'base64').toString('binary');
+};
+
+
+export default class Mediathek {
+
+    constructor() {
+        this.stopped        = true;
+        this.defaultAdapter = undefined;
+    }
+
+    fileNameFromUrl(url) {
+        if (!url) return;
+        const i = url.lastIndexOf('/');
+        return (i > -1) ? url.substring(i+1) : url;
+    }
+
+    //
+    // init
+    //
+
+    static async open() {
+        const mediathek = new this();
+        try { await mediathek.start() } catch (e) { console.log("Mediathek could not be started", e) }
+        return mediathek;
+    }
+
+    // add SFTP/SCP adapter
+    async start() {
+        // add default storage adapters provided by thoregon
+        storageAdapters.set(HttpsStorageAdapter.id, await HttpsStorageAdapter.init());
+        storageAdapters.set(URLReferenceAdapter.id, await URLReferenceAdapter.init());
+        // storageAdapters.set(LocalStorageAdapter.id, await LocalStorageAdapter.init());
+        // storageAdapters.set(IDXDBStorageAdapter.id, await IDXDBStorageAdapter.init());
+        // storageAdapters.set(IPFSStorageAdapter.id,  await IPFSStorageAdapter.init());
+        // storageAdapters.set(TorrentStorageAdapter.id, TorrentStorageAdapter);
+
+        this.defaultAdapter = storageAdapters.get(HttpsStorageAdapter.id);  // todo [REFACTOR]: should be IPFS, add user setting
+        this.stopped        = false;
+    }
+
+    async stop() {
+        this.stopped = true;
+    }
+
+    //
+    // utilities
+    //
+
+    getObjectUrl(surl) {
+        if (surl.startsWith("file:")) {
+
+        } else if (surl.startsWith("http:") || surl.startsWith("http:")) {
+            const url = new URL(surl);
+        }
+    }
+
+    //
+    // API
+    //
+
+    get root() {
+        return me?.mediathek;
+    }
+
+    get tags() {
+        return me?.tags;
+    }
+
+    async getByCID(cid) {
+        const ufd = await this.root.getCID(cid);
+        return ufd;
+    }
+
+    async add(content, { filename, mimetype, storage, credentials, mediathek = true, tags, timeout, onProgress = () => {} } = {}, abortSignal) {
+        // no content, no UFD
+        if (content == undefined || content === '') return;
+
+        const store = this.storageAdapter(storage, { filename, mimetype, storage, credentials, mediathek, tags, timeout, onProgress });     // get the storage adapter
+        ({ content, filename, mimetype, credentials } = await store.adjustItem(content, { filename, mimetype, storage, credentials, mediathek, tags, timeout, onProgress }));
+        let cid     = await store.cid(content, { filename, mimetype, storage, credentials, mediathek, tags, timeout, onProgress });         // get the content id for the content
+
+        let ufd = await this.getByCID(cid);
+        if (ufd && ufd.hasSource()) {
+            // tell the storage adapter that this content should be stored already. adapter is responsible to ensure persistence.
+            // await store.add(content, { prevadd: true, filename, mimetype, credentials, onProgress, abortSignal, timeout });
+            console.log(":CID", cid);
+            return ufd;
+        }
+
+        // todo [OPEN]: determine mimetype if not provided
+
+        const res = await store.add(content, { filename, mimetype, credentials, tags, onProgress, abortSignal, timeout, mediathek });
+        if (res.err) {
+            console.log("Mediathek", res.err);
+            return ;
+        }
+        cid = res.cid;
+
+        // now create the unified file descriptor
+        ufd = ufd ?? await UnifiedFileDescriptor.create({ cid, content: store.contentForUnifiedFileDescriptor(content), name: filename, mimetype, mediathek, tags: tags == undefined ? '' : Array.isArray(tags) ? tags.join(',') : tags.toString() });
+
+        // create and attach a source
+        let source = await store.createSource(cid, { filename, mimetype, credentials, content, use: true, mediathek });
+        if (source) {
+            const sources = ufd.sources;
+            await sources.add(source);
+        }
+
+        // todo: preview
+
+        // managed by mediathek
+        if (mediathek && tags) {
+            await this.maintainTags(cid, tags);
+        }
+
+        // maintain CID registry
+        await this.root.addCID(cid, ufd);
+
+        console.log("+CID", cid);
+
+        return ufd;
+    }
+
+    addSource(cid, content, { filename, mimetype, storage, credentials, mediathek = true, tags, timeout, onProgress = () => {} } = {}, abortSignal) {
+        // todo
+    }
+
+    get(cdi) {
+        // const store = this.storageAdapter(storage);     // get the storage adapter
+
+    }
+
+    async getDataUrl(source) {
+        const url = await source.getUrl();
+        const store = this.getStorageAdapterFor(url);
+        if (!store) return;
+        const b64data = await store.getDataUrl(source);
+        return b64data;
+    }
+
+    /**
+     *
+     * @param cid       cid of resource
+     * @param tags      arrays of tags for this resource
+     * @returns {Promise<void>}
+     */
+    maintainTags(cid, tags) {
+        const mttags = this.tags;
+        for (const tag of tags) {
+            let tagcids = mttags.get(tag);
+            if (!tagcids) {
+                tagcids = Directory.create(); // todo [OPEN]: need persistent 'Set'
+                mttags.put(tag, tagcids);
+            }
+            tagcids.put(cid, 1);
+        }
+    }
+
+    removeTags(cid, tags) {
+        const mttags = this.tags;
+        for (const tag of tags) {
+            let tagcids = mttags.get(tag);
+            if (!tagcids) continue;
+            tagcids.remove(cid);
+        }
+    }
+
+/*
+    async addMT(content, { filename, mimetype, storage, credentials, tags } = {}) {
+        return await this.add(content, { mediathek: true, filename, mimetype, storage, credentials, tags });
+    }
+*/
+
+    async cid(content) {
+        let store = this.storageAdapter(storage);
+        const cid = await store.cid(content);
+        return cid;
+    }
+
+    async allCIDs() {
+        return this.root.allCIDs();
+    }
+
+    async hasCID(cid) {
+        return this.root.hasCID(cid);
+    }
+
+    async exists(content) {
+        let cid = await this.cid(content);
+        return this.existsCID(cid);
+    }
+
+    //
+    // Utils
+    //
+
+    abortController() {
+        // const abortCtrl = new AbortController();
+        // const abortSignal = abortCtrl.signal;
+        return new AbortController();
+    }
+
+    //
+    // storage adapters
+    //
+
+    storageAdapter(storage) {
+        const Store =  this.getStorageAdapter(storage);
+        if (Store) return Store;
+        if (this.defaultAdapter) return this.defaultAdapter;
+        throw ErrNoFileStorageAdapter(storage);
+    }
+
+    /**
+     * add a storage adapter class
+     *
+     * @param adapter
+     */
+    addStorageAdapter(adapter) {
+        if (storageAdapters.has(adapter.id)) throw ErrFileStorageAdapterExists(adapter.id);
+        storageAdapters.set(adapter.id, adapter);
+    }
+
+    getStorageAdapterFor(uri) {
+        const adapter = [...storageAdapters.values()].find(adapter => adapter.responsibleFor(uri));
+        return adapter;
+    }
+
+    getStorageAdapter(id) {
+        return storageAdapters.get(id);
+    }
+
+}
---
+++ libold/unifiedfiledescriptor.mjs
@@ -0,0 +1,118 @@
+/*
+ *
+ * @author: Martin Neitz, Bernhard Lukassen
+ */
+
+import MetaClass           from "/thoregon.archetim/lib/metaclass/metaclass.mjs";
+import ThoregonEntity      from "/thoregon.archetim/lib/thoregonentity.mjs";
+import Directory           from "/thoregon.archetim/lib/directory.mjs";
+import Collection          from "/thoregon.archetim/lib/collection.mjs";
+import FileSource          from "./filesource.mjs";
+
+// import { ATTRIBUTE_MODE } from "/thoregon.archetim/lib/metaclass/metaclass.mjs";
+
+class UnifiedFileDescriptorMeta extends MetaClass {
+
+    initiateInstance() {
+        this.name                     = "UnifiedFileDescriptor";
+        // this.attributeMode            = ATTRIBUTE_MODE.VARENCRYPT;
+
+        this.text("name", { description: 'name of the file', mandatory: true });
+        this.text("description", { description: 'optional description' });
+        this.text("mimetype", { description: 'Mime Type of the file', mandatory: true });
+        this.text("content", { description: 'Mime Type of the file', merge: false, emergent: true });
+        this.image("preview", { embedded: true });
+        // this.boolean("isFolder", { description: 'is it a folder', defaultValue: false });
+        this.boolean("isArchive", { description: 'is it an archive', defaultValue: false });
+        this.text("tags", { description: 'tags for this file' } );
+        this.collection("properties", Directory, { embedded: true, description: 'user/app defined properties' } );
+        this.collection("sources", Collection, { embedded: true, autocomplete: true, description: 'sources where to find the content, source type is the key' });
+    }
+
+}
+
+export default class UnifiedFileDescriptor extends ThoregonEntity() {
+
+    static get ufd() { return true; }
+
+    static async withUrl(name, url, mimetype) {
+        const cid = universe.random();
+        const storage = mediathek.getStorageAdapter('url');
+        const source = await storage.createSource(cid, { name, content: url, mimetype, use: true, mediathek: false });
+
+        const ufd = universe.observe(new UnifiedFileDescriptor());
+        ufd.name = name;
+        ufd.sources = Collection.create();
+        ufd.sources.add(source);
+
+        return ufd;
+    }
+
+    //
+    // Content
+    //
+
+    get readStream() {}
+    get writeStream() {}
+
+    hasSource() {
+        const found = this.sources.find((item) => item.hasUri()) ?? false;
+        return !!found;
+    }
+
+    getAnySource() {
+        const source = this.sources.getAny();
+        return source.item;
+    }
+
+    getAvailableSource() {
+        let source = this.sources.find((item) => item.isAvailable());
+        if (!source && this.cid && this.name) {
+            // assume we can rebuild the resourcesink source
+            source = FileSource.recreateSource(this.cid, { filename: this.name, mimetype: this.mimetype });
+            this.sources.add(source);
+        }
+        return source;
+    }
+
+
+    getSource() {
+        const sources = this.sources;
+        if (!sources) return;
+        if (sources.length === 1) return this.getAnySource();    // if there is only one source, it is the only that 'getAny()' will find
+        let source = sources.find((source) => source.use && !!source.uri);
+        if (!source) return this.getAvailableSource();                 // no source is defined to be used, take any
+        return source;
+    }
+
+    getObjectURL() {
+        return this.getURL();
+    }
+
+    getURL() {
+        const source = this.getSource();
+        return source?.getUrl();
+    }
+
+    async getDataUrl() {
+/*
+        if (this.content) {
+            const b64 = this.content.substring('$B64$'.length);
+            const dataUrl = `data:${this.mimetype};base64,${b64}`;
+            return dataUrl;
+        }
+*/
+        const source = this.getSource();
+        return source
+                ? await mediathek.getDataUrl(source)
+                : undefined;
+    }
+}
+
+export class UnifiedFolderDescriptor extends UnifiedFileDescriptor {
+
+}
+
+UnifiedFileDescriptor.checkIn(import.meta, UnifiedFileDescriptorMeta);
+
+if (globalThis.universe) universe.$UnifiedFileDescriptor = UnifiedFileDescriptor;
